dd81ca12acf7d34b567d4fd2bf459f9bfbd48509
Ved Prakash <87353286+VedxP@users.noreply.github.com>
2022-11-30 12:28:08 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <Nav/>
   <div class="max-w-screen-lg w-full mt-16 slg:mt-20 mb-10 slg:mb-14 mx-auto px-8 py-6 slg:px-24 lg:mt-48">
+    <Nav/>
     <Hero/>
     <main class="grid grid-cols-1 sm:grid-cols-2 slg:grid-cols-3 gap-6 slg:gap-8 pt-8 mx-auto sm:w-11/12 slg:w-full">
       <Content/>

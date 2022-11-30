a4e74b1dfde29f3125ea2d285445fbb319541e40
Ved Prakash <87353286+VedxP@users.noreply.github.com>
2022-11-30 13:16:29 +0530

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,9 +5,9 @@
       <svg fill="none" stroke="currentColor" class="w-6 h-6 slg:w-9 slg:h-9 lg:w-12 lg:h-12" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"/></svg>
     </div>
     <!--Links-->
-    <span class="flex text-sm items-center gap-1 sm:gap-2 font-bold">
+    <div class="flex text-sm items-center gap-1 sm:gap-2 font-bold">
       <a class="shrink hover:bg-button px-3 py-2 slg:px-5 slg:py-3 rounded-md" href="https://github.com/VedxP/battery">Factory</a>
       <a class="shrink hover:bg-button px-3 py-2 sm:px-4 slg:px-5 slg:py-3 rounded-md" href="/https://github.com/VedxP">Manager</a>
-    </span>
+    </div>
   </nav>
 </template>

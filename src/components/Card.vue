f724a0246934849a208c3feca9b1b7c29a7eb8a1
VedxP <87353286+VedxP@users.noreply.github.com>
2022-11-30 15:31:11 +0530

    cleanups

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grow flex flex-col p-6 py-6 items-center rounded-2xl bg-card text-content text-sm slg:text-md leading-snug tracking-tight slg:tracking-normal">
+    <div class="grow flex items-center p-6 rounded-2xl bg-card text-content text-sm slg:text-md leading-snug tracking-tight slg:tracking-normal">
     <slot/>
   </div>
 </template>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,11 @@
 <template>
-  <nav class="bg-nav px-6 py-2 slg:py-4 slg:px-12 flex items-center justify-between fixed w-full">
-    <!--Logo-->
-    <div class="grow left-0">
+  <nav class="bg-nav px-6 py-4 slg:py-8 slg:px-12 top-0 flex items-center fixed w-full">
+    <div class="grow">
       <svg fill="none" stroke="currentColor" class="w-6 h-6 slg:w-9 slg:h-9 lg:w-12 lg:h-12" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"/></svg>
     </div>
-    <!--Links-->
-    <span class="flex items-center text-sm gap-1 font-bold">
+    <div class="text-sm gap-1 font-bold">
       <a class="shrink hover:bg-button px-3 py-2 slg:px-5 slg:py-3 rounded-md" href="https://github.com/VedxP/battery">Factory</a>
       <a class="shrink hover:bg-button px-3 py-2 slg:px-5 slg:py-3 rounded-md" href="/https://github.com/VedxP">Manager</a>
-    </span>
+    </div>
   </nav>
 </template>

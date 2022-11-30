65703aada12cdd450332bf14caea462c05f5425d
Ved Prakash <87353286+VedxP@users.noreply.github.com>
2022-11-30 13:15:21 +0530

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,13 @@
 <template>
   <nav class="bottom-0 px-3 py-3 flex justify-between items-center fixed w-full bg-nav backdrop-blur-md sm:top-0 sm:bottom-auto sm:px-6 lg:py-4">
     <!--Logo-->
-    <div class="grow h-[1px] top-0 absolute left-0 w-full bg-topbar lg:h-1">
+    <div class="grow h-[1px] top-0 absolute left-0 w-full lg:h-1">
       <svg fill="none" stroke="currentColor" class="w-6 h-6 slg:w-9 slg:h-9 lg:w-12 lg:h-12" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"/></svg>
     </div>
     <!--Links-->
-    <span class="flex items-center text-sm gap-1 font-bold">
+    <span class="flex text-sm items-center gap-1 sm:gap-2 font-bold">
       <a class="shrink hover:bg-button px-3 py-2 slg:px-5 slg:py-3 rounded-md" href="https://github.com/VedxP/battery">Factory</a>
-      <a class="shrink hover:bg-button px-3 py-2 slg:px-5 slg:py-3 rounded-md" href="/https://github.com/VedxP">Manager</a>
+      <a class="shrink hover:bg-button px-3 py-2 sm:px-4 slg:px-5 slg:py-3 rounded-md" href="/https://github.com/VedxP">Manager</a>
     </span>
   </nav>
 </template>

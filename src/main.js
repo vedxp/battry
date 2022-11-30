b397b3533b4f66a0166a179dc3085440ae2e3987
VedxP <87353286+VedxP@users.noreply.github.com>
2022-11-30 17:41:01 +0530

    ssg

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg/single-page'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+export const createApp = ViteSSG(App)
